2615ef982fdaf96f3e7530ac093c7d9b7a332093
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2021-03-10 22:45:19 +0100

    Fetches covid data from ourworldindata if it's missing;

--- Python/fetch.py
+++ Python/fetch.py
@@ -1,2 +1,15 @@
-def hello_world():
-    print("hello")
+import pandas as pd
+import requests
+import os.path
+
+DATA_URL = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+RAW_FILE_PATH = "data/raw_file.csv"
+
+def fetch_raw_data_if_missing():
+    if os.path.isfile(RAW_FILE_PATH):
+        return
+    file_request = requests.get(DATA_URL, allow_redirects=True)
+    with open(RAW_FILE_PATH, 'wb') as raw_file: 
+        raw_file.write(file_request.content)
+
+fetch_raw_data_if_missing()
