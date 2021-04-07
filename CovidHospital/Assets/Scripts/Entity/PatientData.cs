b51320ad39043bb26b939bf71038a77118702621
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-04-07 20:22:07 +0200

    Adds serializable pawn models

---
+++ CovidHospital/Assets/Scripts/Entity/PatientData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Entity
+{
+    [Serializable]
+    public class PatientData : ScriptableObject
+    {
+        public int[] conditionsId;
+
+        public float mentalHealth;
+        public float comfort;
+
+        public float covidProgress;
+    }
+}
---
+++ CovidHospital/Assets/Scripts/Entity/PawnController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Entity
+{
+    public abstract class Pawn : MonoBehaviour
+    {
+        private PawnData _pawnData;
+        private Sprite _head;
+        private Sprite _body;
+    }
+
+    public abstract class PatientPawn : Pawn
+    {
+        //etc.
+    }
+
+    public abstract class PersonnelPawn : Pawn
+    {
+        //etc.
+    }
+}
