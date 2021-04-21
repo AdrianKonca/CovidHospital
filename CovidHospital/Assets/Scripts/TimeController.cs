44ed92ab2d8f7c15d51588a57d8b541219dc73ae
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-04-21 18:17:25 +0200

    Adds time

--- CovidHospital/Assets/Scripts/TimeController.cs
+++ CovidHospital/Assets/Scripts/TimeController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.Experimental.Rendering.Universal;
 using UnityEngine.PlayerLoop;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.Tilemaps;
@@ -11,13 +13,18 @@ public class TimeController : MonoBehaviour
 {
     private float _dayLength = 120f;
     private float _currentTime;
-    private long dayCount;
+    private long _dayCount;
     private bool _isDay;
+    public GameObject Light;
 
+    private void Awake()
+    {
+    }
 
     private void Update()
     {
         _currentTime += Time.deltaTime / _dayLength;
+        Light.GetComponent<Light2D>().intensity = _currentTime * 10;
         Debug.Log(_currentTime);
     }
 }
