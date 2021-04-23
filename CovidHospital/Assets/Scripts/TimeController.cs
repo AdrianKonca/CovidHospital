574be2916eb35a55ac11c9c345c8bd8611052620
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-04-23 16:00:20 +0200

    Adds events in timecontroller

--- CovidHospital/Assets/Scripts/TimeController.cs
+++ CovidHospital/Assets/Scripts/TimeController.cs
@@ -4,6 +4,22 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class TimeController : MonoBehaviour
 {
+    public event OnHourIncrease OnHourEvent;
+
+    public event OnMinuteIncrease OnMinuteEvent;
+
+    public event OnDayIncrease OnDayEvent;
+
+    public event EventHandler OnDayEnter;
+
+    public event EventHandler OnNightEnter;
+
+    public delegate void OnHourIncrease(int h);
+
+    public delegate void OnMinuteIncrease(int m);
+
+    public delegate void OnDayIncrease(long d);
+
     public long dayCount;
     public bool isDay;
     public int hours;
@@ -33,21 +49,34 @@ public class TimeController : MonoBehaviour
     {
         hours = (int) (24f * _currentTime);
         minutes = (int) (24f * _currentTime % 1 * 60);
+
+        OnHourEvent?.Invoke(hours);
+        OnMinuteEvent?.Invoke(minutes);
     }
 
     private void CheckForNextDay()
     {
+        var dayStart = 0.25;
+        var dayEnd = 0.85;
+
         if (_currentTime >= 1)
         {
             _currentTime = 0;
             dayCount++;
-            NextDayAction();
+            OnDayEvent?.Invoke(dayCount);
         }
 
-        if (0.25 < _currentTime & _currentTime < 0.85)
+        if (dayStart < _currentTime & _currentTime < dayEnd & !isDay)
+        {
             isDay = true;
-        else 
+            OnDayEnter?.Invoke(this, EventArgs.Empty);
+        }
+
+        if (dayEnd < _currentTime & isDay)
+        {
             isDay = false;
+            OnNightEnter?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     private float GetLightIntensity()
@@ -55,9 +84,5 @@ public class TimeController : MonoBehaviour
         float Intensity = (float) Math.Pow(Math.Sin((_currentTime - 0.05) * Math.PI), 2) * 1.1f;
         debugLight = Intensity;
         return Mathf.Clamp(Intensity, 0.2f, 1f);
-    }
-
-    private void NextDayAction()
-    {
     }
 }
