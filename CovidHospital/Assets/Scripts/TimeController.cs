0b29ab1cdde7842eb0f298589f48c900c6c05954
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-04-23 19:11:52 +0200

    Rename events in TimeController

--- CovidHospital/Assets/Scripts/TimeController.cs
+++ CovidHospital/Assets/Scripts/TimeController.cs
@@ -4,29 +4,28 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class TimeController : MonoBehaviour
 {
-    public event OnHourIncrease OnHourEvent;
+    public event OnHourIncreaseDelegate OnHourIncrease;
 
-    public event OnMinuteIncrease OnMinuteEvent;
+    public event OnMinuteIncreaseDelegate OnMinuteIncrease;
 
-    public event OnDayIncrease OnDayEvent;
+    public event OnDayIncreaseDelegate OnDayIncrease;
 
     public event EventHandler OnDayEnter;
 
     public event EventHandler OnNightEnter;
 
-    public delegate void OnHourIncrease(int h);
+    public delegate void OnHourIncreaseDelegate(int h);
 
-    public delegate void OnMinuteIncrease(int m);
+    public delegate void OnMinuteIncreaseDelegate(int m);
 
-    public delegate void OnDayIncrease(long d);
+    public delegate void OnDayIncreaseDelegate(long d);
 
     public long dayCount;
     public bool isDay;
     public int hours;
     public int minutes;
-    public float debugLight;
 
-    private const float DayLength = 120f;
+    private const float DayLength = 30f;
     private float _currentTime;
 
     public GameObject Light;
@@ -50,20 +49,20 @@ public class TimeController : MonoBehaviour
         hours = (int) (24f * _currentTime);
         minutes = (int) (24f * _currentTime % 1 * 60);
 
-        OnHourEvent?.Invoke(hours);
-        OnMinuteEvent?.Invoke(minutes);
+        OnHourIncrease?.Invoke(hours);
+        OnMinuteIncrease?.Invoke(minutes);
     }
 
     private void CheckForNextDay()
     {
-        var dayStart = 0.25;
-        var dayEnd = 0.85;
+        const double dayStart = 0.25;
+        const double dayEnd = 0.85;
 
         if (_currentTime >= 1)
         {
             _currentTime = 0;
             dayCount++;
-            OnDayEvent?.Invoke(dayCount);
+            OnDayIncrease?.Invoke(dayCount);
         }
 
         if (dayStart < _currentTime & _currentTime < dayEnd & !isDay)
@@ -82,7 +81,6 @@ public class TimeController : MonoBehaviour
     private float GetLightIntensity()
     {
         float Intensity = (float) Math.Pow(Math.Sin((_currentTime - 0.05) * Math.PI), 2) * 1.1f;
-        debugLight = Intensity;
         return Mathf.Clamp(Intensity, 0.2f, 1f);
     }
 }
