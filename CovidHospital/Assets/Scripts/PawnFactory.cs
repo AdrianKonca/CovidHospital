25023040ea01744647ba31c3e619b00e68ce80a6
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2021-06-03 21:00:22 +0200

    Appends previous commit;

---
+++ CovidHospital/Assets/Scripts/PawnFactory.cs
@@ -0,0 +1,45 @@
+﻿using Entity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.U2D;
+
+public enum BodyPart { Hair, Head, Body }
+public enum Direction { Front, Back, Right, Left }
+
+public class PawnFactory : MonoBehaviour
+{
+    public GameObject PatientPrefab;
+    public GameObject DoctorPrefab;
+    public GameObject NursePrefab;
+
+    public TimeController TimeController;
+    public NurseManager NurseManager;
+
+    // Update is called once per frame
+    private bool _loaded = false;
+    void Update()
+    {
+        if (SpriteManager.AllSpritesLoaded && _loaded == false)
+        {
+            _loaded = true;
+            var patient = Instantiate(PatientPrefab);
+            var pc = patient.GetComponent<PawnController>();
+            pc.Initialize(Role.Patient, TimeController, NurseManager);
+        }
+    }
+
+    public void Pawn(Vector3 coordinates)
+    {
+        if (!SpriteManager.AllSpritesLoaded)
+            return;
+        var patient = Instantiate(PatientPrefab);
+        var pc = patient.GetComponent<PawnController>();
+        pc.Initialize(Role.Patient, TimeController, NurseManager);
+        pc.transform.position = coordinates;
+    }
+}
