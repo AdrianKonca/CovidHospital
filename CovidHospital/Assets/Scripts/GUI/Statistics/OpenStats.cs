433f3f85d46fec461ef37c80ec30fdd40a2c1612
Loleko <46678137+tj44531@users.noreply.github.com>
2021-06-04 15:04:47 +0200

    creating stats window

---
+++ CovidHospital/Assets/Scripts/GUI/Statistics/OpenStats.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class OpenStats : MonoBehaviour
+{
+    Text txt;
+    public GameObject stats;
+    private void Start()
+    {
+        if (stats.activeSelf)
+            stats.SetActive(false);
+    }
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit;
+            if (hit = Physics2D.Raycast(mousePos2D, Vector2.zero))
+            {
+                if (hit.rigidbody)
+                {
+                    openStatistics(hit);
+                }
+            }
+        }
+    }
+
+    private void openStatistics(RaycastHit2D hit)
+    {
+        if (stats.activeSelf)
+            stats.SetActive(false);
+        else
+            stats.SetActive(true);
+        txt = stats.transform.GetChild(1).GetComponent<Text>();
+        //txt.text = hit.transform.gameObject.GetComponent<>();
+        
+    }
+    public void CloseWindow()
+    {
+        stats.SetActive(false);
+    }
+}
+
---
+++ CovidHospital/Assets/Scripts/GUI/Statistics/StatisticsGUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatisticsGUI : MonoBehaviour
+{
+    public GameObject statistics;
+
+
+
+    public void Exit()
+    {
+        statistics.SetActive(false);
+    }
+}
