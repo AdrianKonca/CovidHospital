613f562b6c7f48cfed0305d2d7785bb9e9f125cf
Loleko <46678137+tj44531@users.noreply.github.com>
2021-06-05 14:35:12 +0200

    statistics window

--- CovidHospital/Assets/Scripts/GUI/Statistics/OpenStats.cs
+++ CovidHospital/Assets/Scripts/GUI/Statistics/OpenStats.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Entity;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,6 +7,7 @@ using UnityEngine.UI;
 
 public class OpenStats : MonoBehaviour
 {
+    PatientController ctrl = null ;
     Text txt;
     public GameObject stats;
     private void Start()
@@ -15,6 +17,20 @@ public class OpenStats : MonoBehaviour
     }
     private void Update()
     {
+        if (ctrl)
+        {
+            var hunger = ctrl.patientData.hunger;
+            var hygiene = ctrl.patientData.hygiene;
+            var toilet = ctrl.patientData.toilet;
+            var covid = ctrl.patientData.covidProgress;
+            var comfort = ctrl.patientData.comfort;
+            SetValue(2, hunger);
+            SetValue(5, hygiene);
+            SetValue(6, toilet);
+            SetValue(3, covid);
+            SetValue(4, comfort);
+        }
+        
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
@@ -32,16 +48,29 @@ public class OpenStats : MonoBehaviour
 
     private void openStatistics(RaycastHit2D hit)
     {
-        if (stats.activeSelf)
-            stats.SetActive(false);
-        else
+        if (!stats.activeSelf)
             stats.SetActive(true);
+
         txt = stats.transform.GetChild(1).GetComponent<Text>();
-        //txt.text = hit.transform.gameObject.GetComponent<>();
-        
+
+        string name = hit.transform.name;
+        ctrl = hit.transform.gameObject.GetComponent<PatientController>();
+        var age = ctrl.PawnData.age;
+        txt.text = name + " (" + age + ")";
+
     }
+
+    private void SetValue(int slider, float value)
+    {
+        stats.transform.GetChild(slider).GetChild(1).GetComponent<Slider>().maxValue = 100;
+        stats.transform.GetChild(slider).GetChild(1).GetComponent<Slider>().value = value;
+        stats.transform.GetChild(slider).GetChild(1).GetChild(1).GetChild(0).GetComponent<Image>().color = Color.Lerp(Color.red, Color.green, value/100);
+        stats.transform.GetChild(slider).GetChild(1).GetChild(2).GetComponent<Text>().text = ((int)value).ToString() + " %";
+    }
+
     public void CloseWindow()
     {
+        ctrl = null;
         stats.SetActive(false);
     }
 }
